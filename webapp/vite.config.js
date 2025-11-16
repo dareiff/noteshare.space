af3bfdef15580a79f829b0dd18ff2c808af85254
Derek Reiff <d@derekr.net>
2025-11-16 00:58:22 -0800

    Docker builds

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { plugin as markdown } from 'vite-plugin-markdown';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		markdown({ mode: ['html'] })
+	],
 	optimizeDeps: {
 		include: ['highlight.js', 'highlight.js/lib/core']
 	},
